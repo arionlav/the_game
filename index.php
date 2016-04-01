6a15f488aaf6f925a6561a57a0387ad60915d5e9
arionlav <aszxas@i.ua>
2016-04-01 22:46:05 +0400

    add instructions

--- index.php
+++ index.php
@@ -4,3 +4,8 @@ require_once ('core/bootstrap.php');
 
 // add comment
 // test branch
+// создаем репозиторий (например, импортируем из удаленного репозитория
+// коммитим изменнеия
+// в окне Version control - Incoming вкладка, жмем Update project,
+// там URL выбираем, в свертке branches, во всплывшем окне, выбираем существующую ветку и заливаем туда
+// например https://github.com/arionlav/the_game/branches/test_branch
