0a79e121939b7b47e86e4ad90ea0fe22f7a56531
arionlav <aszxas@i.ua>
2016-04-02 21:50:57 +0400

    change index.php

--- index.php
+++ index.php
@@ -4,3 +4,8 @@ require_once ('core/bootstrap.php');
 
 // add comment
 // must be alexes branch
+
+// VCN -> Browse SVN repository -> Browse Subversion repository
+// in branches list right click -> Branch or tag... -> create new branch
+// in branches list right click -> Export -> select new folder
+// in branches list right click -> checkout -> select folder and Destination
